1aab62a65ead256205e1c9bd1b0edc3f372da853
smlhbt88 <samuel.habte@cognizant.com>
2021-01-22 13:15:53 -0800

    User stories added and refactoring done

--- src/test/java/com/vending/machine/VendingMachineTest.java
+++ src/test/java/com/vending/machine/VendingMachineTest.java
@@ -13,114 +13,154 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class VendingMachineTest {
-
+    VendingMachine vendingMachine_no_arg = new VendingMachine();
+    Customer customer_balance_arg = new Customer(3);
+    VendingMachine vendingMachine_customer_arg = new VendingMachine(customer_balance_arg);
+    VendingMachine vendingMachine_balance_arg = new VendingMachine(1000);
+    Administrator administrator = new Administrator(vendingMachine_balance_arg);
+    /**
+     * Given there are no items in stock
+     * When I check the vending machine
+     * Then I should see it is empty
+     */
     @Test
     public void checkNoItemsInStockIsEmpty_Administrator() {
-        VendingMachine vendingMachine = new VendingMachine();
-        assertTrue(vendingMachine.checkItemsIsEmpty());
+        assertTrue(vendingMachine_no_arg.checkItemsIsEmpty());
     }
 
+    /**
+     * Given there are no items in stock
+     * When I add some items
+     * Then I should see them in the vending machine
+     */
     @Test
     public void addItemsToVendingMachine_Administrator() {
-        VendingMachine vendingMachine = new VendingMachine();
         Item item1 = new Item(1,"Chocolate",4.99);
         Item item2 = new Item(2, "Coca-Cola", 2.49);
-        vendingMachine.addItemsToTheStock(item1);
-        vendingMachine.addItemsToTheStock(item2);
-        assertEquals(item1,vendingMachine.getItems().get(0));
-        assertEquals(item2,vendingMachine.getItems().get(1));
+        vendingMachine_no_arg.addItemsToTheStock(item1);
+        vendingMachine_no_arg.addItemsToTheStock(item2);
+        assertEquals(item1,vendingMachine_no_arg.getItems().get(0));
+        assertEquals(item2,vendingMachine_no_arg.getItems().get(1));
     }
 
+    /**
+     * Given there are items in stock
+     * When I check what is available
+     * Then I should see the name, price and code of the items
+     */
     @Test
     public void checkAvailableItemsInStock_Customer() {
-        VendingMachine vendingMachine = new VendingMachine();
         Item item1 = new Item(1,"Chocolate",4.99);
         Item item2 = new Item(2, "Coca-Cola", 2.49);
-        vendingMachine.addItemsToTheStock(item1);
-        vendingMachine.addItemsToTheStock(item2);
+        vendingMachine_no_arg.addItemsToTheStock(item1);
+        vendingMachine_no_arg.addItemsToTheStock(item2);
         List<String> expectedItemList = new ArrayList<>();
         expectedItemList.add("Item{itemCode=1, itemName='Chocolate', price=4.99}");
         expectedItemList.add("Item{itemCode=2, itemName='Coca-Cola', price=2.49}");
-        assertEquals(expectedItemList.get(0),vendingMachine.getItems().get(0).toString());
-        assertEquals(expectedItemList.get(1),vendingMachine.getItems().get(1).toString());
+        assertEquals(expectedItemList.get(0),vendingMachine_no_arg.getItems().get(0).toString());
+        assertEquals(expectedItemList.get(1),vendingMachine_no_arg.getItems().get(1).toString());
 
     }
 
+    /**
+     * Given I have inserted some amount of money
+     * When I enter a non-existent item code
+     * Then the vending machine shows no such item
+     */
     @Test
     public void checkIfNonExistentItemInStock_Customer() {
-        Customer customer = new Customer(100);
-        VendingMachine vendingMachine = new VendingMachine(customer);
-        assertTrue(customer.checkIfCustomerHasBalance());
+        assertTrue(customer_balance_arg.checkIfCustomerHasBalance());
         Item item1 = new Item(1,"Chocolate",4.99);
         Item item2 = new Item(2, "Coca-Cola", 2.49);
-        vendingMachine.addItemsToTheStock(item1);
-        vendingMachine.addItemsToTheStock(item2);
-        assertEquals("no such item",vendingMachine.checkIfItemExists(3));
+        vendingMachine_customer_arg.addItemsToTheStock(item1);
+        vendingMachine_customer_arg.addItemsToTheStock(item2);
+        assertEquals("no such item",vendingMachine_customer_arg.checkIfItemExists(3));
     }
 
+    /**
+     * Given I have inserted some amount of money
+     * When I enter an item code of an item that costs more that I have inserted
+     * Then the vending machine shows insufficient fund
+     */
     @Test
     public void insufficientFundWhenBuyingAnItem_Customer() {
-        Customer customer = new Customer(3);
-        VendingMachine vendingMachine = new VendingMachine(customer);
         Item item1 = new Item(1,"Chocolate",4.99);
         Item item2 = new Item(2, "Coca-Cola", 2.49);
-        vendingMachine.addItemsToTheStock(item1);
-        vendingMachine.addItemsToTheStock(item2);
-        assertEquals("insufficient fund",vendingMachine.checkInsufficientFund(1));
+        vendingMachine_customer_arg.addItemsToTheStock(item1);
+        vendingMachine_customer_arg.addItemsToTheStock(item2);
+        assertEquals("insufficient fund",vendingMachine_customer_arg.checkInsufficientFund(1));
     }
 
+    /**
+     * Given I have inserted some amount of money
+     * When I enter an item code of an item that costs less that I have inserted
+     * Then I get the item and the change
+     */
     @Test
     public void getItemAndChangeIfHadBalance_Customer() {
-        Customer customer = new Customer(3);
-        VendingMachine vendingMachine = new VendingMachine(customer);
         Item item1 = new Item(1,"Chocolate",4.99);
         Item item2 = new Item(2, "Coca-Cola", 2.49);
-        vendingMachine.addItemsToTheStock(item1);
-        vendingMachine.addItemsToTheStock(item2);
+        vendingMachine_customer_arg.addItemsToTheStock(item1);
+        vendingMachine_customer_arg.addItemsToTheStock(item2);
         Map<Item, BigDecimal> changeMap = new HashMap<>();
         MathContext mc = new MathContext(2);
         changeMap.put(item2,new BigDecimal(0.51).round(mc));
-        assertEquals(changeMap.get(item2),vendingMachine.getItemAndChange(2).get(item2));
-        assertEquals(changeMap,vendingMachine.getItemAndChange(2));
+        assertEquals(changeMap.get(item2),vendingMachine_customer_arg.getItemAndChange(2).get(item2));
+        assertEquals(changeMap,vendingMachine_customer_arg.getItemAndChange(2));
     }
 
+    /**
+     * Given I have inserted some amount of money
+     * When I press the cancel button
+     * Then I get the full amount refunded
+     */
     @Test
     public void cancelItemFullAmountRefunded_Customer() {
-        Customer customer = new Customer(3);
-        VendingMachine vendingMachine = new VendingMachine(customer);
         Item item1 = new Item(1,"Chocolate",4.99);
         Item item2 = new Item(2, "Coca-Cola", 2.49);
-        vendingMachine.addItemsToTheStock(item1);
-        vendingMachine.addItemsToTheStock(item2);
-        assertEquals(3,vendingMachine.cancelTransaction(),0);
+        vendingMachine_customer_arg.addItemsToTheStock(item1);
+        vendingMachine_customer_arg.addItemsToTheStock(item2);
+        assertEquals(3,vendingMachine_customer_arg.cancelTransaction(),0);
     }
 
+    /**
+     * Given there are some balance
+     * When I check the balance of the vending machine
+     * Then I should see the current balance
+     */
     @Test
     public void checkBalanceOfVendingMachine_Administrator() {
-        VendingMachine vendingMachine = new VendingMachine(1000);
-        assertEquals(1000,vendingMachine.getBalance(),0);
+        assertEquals(1000,vendingMachine_balance_arg.getBalance(),0);
     }
 
+    /**
+     * Given I have inserted some money into the vending machine
+     * When I check amount inserted
+     * Then I should see the amount
+     */
     @Test
     public void checkBalance_Customer() {
-        Customer customer = new Customer(100);
-        assertEquals(100,customer.getBalance(),0);
+        assertEquals(3,customer_balance_arg.getBalance(),0);
     }
 
+    /**
+     * When I withdraw money from the vending machine
+     * Then I should see the balance decrease
+     */
     @Test
     public void withdrawMoneyFromMachine_Administrator() {
-        VendingMachine vendingMachine=new VendingMachine(1000);
-        Administrator administrator = new Administrator(vendingMachine);
         administrator.withdrawMoney(100);
-        assertEquals(900, vendingMachine.getBalance(),0);
+        assertEquals(900, vendingMachine_balance_arg.getBalance(),0);
     }
 
+    /**
+     *When I deposit money to the vending machine
+     * Then I should see the balance increase
+     */
     @Test
     public void depositMoneyFromMachine_Administrator() {
-        VendingMachine vendingMachine=new VendingMachine(1000);
-        Administrator administrator = new Administrator(vendingMachine);
         administrator.depositMoney(200);
-        assertEquals(1200, vendingMachine.getBalance(),0);
+        assertEquals(1200, vendingMachine_balance_arg.getBalance(),0);
     }
 
     /**
@@ -132,15 +172,13 @@ public class VendingMachineTest {
      */
     @Test
     public void purchaseAnItem_Customer() {
-        Customer customer = new Customer(20);
-        VendingMachine vendingMachine = new VendingMachine(customer);
         Item item1 = new Item(1,"Chocolate",4.99);
         Item item2 = new Item(2, "Coca-Cola", 2.49);
-        vendingMachine.addItemsToTheStock(item1);
-        vendingMachine.addItemsToTheStock(item2);
-        assertEquals(item1,vendingMachine.getItemByItemCode(1));
-        assertEquals(1,vendingMachine.getItems().size());
-        assertEquals(4.99,vendingMachine.getBalance(),0);
+        vendingMachine_customer_arg.addItemsToTheStock(item1);
+        vendingMachine_customer_arg.addItemsToTheStock(item2);
+        assertEquals(item1,vendingMachine_customer_arg.getItemByItemCode(1));
+        assertEquals(1,vendingMachine_customer_arg.getItems().size());
+        assertEquals(4.99,vendingMachine_customer_arg.getBalance(),0);
 
     }
 }
